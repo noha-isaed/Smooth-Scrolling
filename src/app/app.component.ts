1a566789d2e3a9d16b9f5fa2c1f37c000f739721
noha-isaed <47281464+noha-isaed@users.noreply.github.com>
2021-06-13 20:19:35 +0300

    edit in duration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,24 @@ export class AppComponent implements OnInit{
   title = 'SmoothScrolling';
 
   public Events: Array<any> = [
-    {"index": 0,"type": 0, "description": "Click event"},
-    {"index": 1,"type": 0,"description": "Click event"},
-    {"index": 2,"type": 1,"description": "Visit new page event"},
-    {"index": 3,"type": 0,"description": "Click event"},
-    {"index": 4,"type": 0,"description": "Click event"},
-    {"index": 5,"type": 0,"description": "Click event"},
-    {"index": 6,"type": 2,"description": "Scroll event"},
-    {"index": 7,"type": 0,"description": "Click event"},
-    {"index": 8,"type": 1,"description": "Visit new page event"},
-    {"index": 9,"type": 1,"description": "Visit new page event"},
-    {"index": 10,"type": 2,"description": "Scroll event"},
-    {"index": 11,"type": 2,"description": "Scroll event"},
-    {"index": 12,"type": 0,"description": "Click event"},
-    {"index": 13,"type": 1,"description": "Visit new page event event"}
+    {"index": 0,"type": 0, "duration": 3500, "description": "Click event" },
+    {"index": 1,"type": 0, "duration": 3000, "description": "Click event"},
+    {"index": 2,"type": 1, "duration": 500, "description": "Visit new page event"},
+    {"index": 3,"type": 0, "duration": 700,"description": "Click event"},
+    {"index": 4,"type": 0, "duration": 4000, "description": "Click event"},
+    {"index": 5,"type": 0, "duration": 2000, "description": "Click event"},
+    {"index": 6,"type": 2, "duration": 800, "description": "Scroll event"},
+    {"index": 7,"type": 0, "duration": 1500, "description": "Click event"},
+    {"index": 8,"type": 1, "duration": 500,  "description": "Visit new page event"},
+    {"index": 9,"type": 1, "duration": 900, "description": "Visit new page event"},
+    {"index": 10,"type": 2, "duration": 3500, "description": "Scroll event"},
+    {"index": 11,"type": 2, "duration": 2700, "description": "Scroll event"},
+    {"index": 12,"type": 0, "duration": 3200, "description": "Click event"},
+    {"index": 13,"type": 1, "duration": 5000, "description": "Visit new page event event"}
   ]
 
 
-
+  public time: Array<number> =  [3500,300,500,700,4000,2000,800,1500,500,900,3500,2700,3200,5000]
   ngOnInit(): void {
 
     for (let i = 0 ; i < this.Events.length ; i++){
@@ -42,31 +42,32 @@ export class AppComponent implements OnInit{
       if(this.Events[i].type == 0  ) {this.Events[i].icon = "fa fa-hand-pointer-o" }
       else  if(this.Events[i].type == 1 ) {this.Events[i].icon = "fa fa-mouse-pointer" }
       else  if(this.Events[i].type == 2 ) {this.Events[i].icon = "fa fa-angle-double-down" }
-      console.log(this.Events[i])
     }
 
     
   }
-  public  counter:number = -1;
+  public  counter:number = 0;
+  
 
   NavigateEvents(anchor: string): void{
     let top = 0 ;
-    const interval = setInterval(() => {
-      this.counter++;
-     // console.log(this.counter);
-      if (this.counter >= this.Events.length  ) {
-        clearInterval(interval);
-      }
-      else{
-        if( this.counter > 6){
+
+        const interval = setInterval(() => {
+        console.log(this.Events[this.counter].duration);
+        this.counter++;
+        if (this.counter >= this.Events.length  ) {
+          clearInterval(interval);
+        }
+        if(this.counter > 6){
           let theDiv: HTMLElement = document.getElementById("container")   as HTMLElement;  
           top++;
           theDiv.scrollTop = 60*top;
-        }    
-      }
-     
-    }, 1000);
+      }    
+       
+      }, this.Events[this.counter].duration);
 
-  }
+    }
+  
+  
   
 }
