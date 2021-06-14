e630e9a14103ab88b01e3a1d7fe3d8fb50463fe7
noha-isaed <47281464+noha-isaed@users.noreply.github.com>
2021-06-14 18:23:35 +0300

    edit in duration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,12 @@ export class AppComponent implements OnInit{
     {"index": 10,"type": 2, "duration": 3500, "description": "Scroll event"},
     {"index": 11,"type": 2, "duration": 2700, "description": "Scroll event"},
     {"index": 12,"type": 0, "duration": 3200, "description": "Click event"},
-    {"index": 13,"type": 1, "duration": 5000, "description": "Visit new page event event"}
+    {"index": 13,"type": 1, "duration": 600, "description": "Visit new page event event"}
   ]
 
 
   public time: Array<number> =  [3500,300,500,700,4000,2000,800,1500,500,900,3500,2700,3200,5000]
+  
   ngOnInit(): void {
 
     for (let i = 0 ; i < this.Events.length ; i++){
@@ -47,27 +48,33 @@ export class AppComponent implements OnInit{
     
   }
   public  counter:number = -1;
-  
-  
-  NavigateEvents(anchor: string): void{
-    let top = 0 ;
-    this.counter++;
-        const interval = setInterval(() => {
-        console.log(this.Events[this.counter].duration);
-        this.counter++;
-        if (this.counter >= this.Events.length  ) {
-          clearInterval(interval);
-        }
-        if(this.counter > 6){
-          let theDiv: HTMLElement = document.getElementById("container")   as HTMLElement;  
-          top++;
-          theDiv.scrollTop = 60*top;
-      }    
-       
-      }, this.Events[this.counter].duration);
 
-    }
-  
-  
-  
-}
+  NavigateEvents(anchor: string): void{
+    
+       let top = 0 ;
+       this.counter++;
+
+      let interval1 = setInterval(() => {
+          
+              
+        if (this.counter == this.Events.length ) {
+          clearInterval(interval1);
+        }
+        else{
+         setTimeout(() => {
+            console.log(this.counter);
+            console.log(this.Events[this.counter].duration);
+
+            if(this.counter > 6){
+              let theDiv: HTMLElement = document.getElementById("container")   as HTMLElement;  
+              top++;
+              theDiv.scrollTop = 80*top;
+            }
+            this.counter++;
+          
+          }, this.Events[this.counter].duration);  
+        }
+      }, 1000); 
+  }
+
+  }
